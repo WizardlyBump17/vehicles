afab10b74f018b86e7421ddc3a421a38d62e2ba3
WizardlyBump17 <davicraft130@gmail.com>
2022-07-30 19:43:58 -0300

    added airplanes

---
+++ api/src/main/java/com/wizardlybump17/vehicles/api/model/airplane/AirplaneModel.java
@@ -0,0 +1,98 @@
+package com.wizardlybump17.vehicles.api.model.airplane;
+
+import com.ticxo.modelengine.api.ModelEngineAPI;
+import com.ticxo.modelengine.api.model.ActiveModel;
+import com.ticxo.modelengine.api.model.ModeledEntity;
+import com.ticxo.modelengine.api.model.mount.handler.IMountHandler;
+import com.wizardlybump17.vehicles.api.Vehicles;
+import com.wizardlybump17.vehicles.api.entity.AirplaneEntity;
+import com.wizardlybump17.vehicles.api.model.VehicleModel;
+import com.wizardlybump17.vehicles.api.vehicle.airplane.Airplane;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.configuration.serialization.SerializableAs;
+import org.bukkit.craftbukkit.v1_17_R1.CraftWorld;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+
+@SerializableAs("airplane")
+@Getter
+@Setter
+public class AirplaneModel extends VehicleModel<Airplane> {
+
+    private float pitchSpeed;
+    private float maxPitch;
+    private float minPitch;
+    private float minFallSpeed;
+
+    public AirplaneModel(Vehicles plugin, String name, double maxSpeed, @NonNull Map<Double, Double> acceleration, Map<Double, Double> damage, @NonNull Map<Double, Double> breakForce, @NonNull String megModel, float rotationSpeed, float jumpHeight, float minPitch, float maxPitch, float pitchSpeed, float minFallSpeed) {
+        super(plugin, name, maxSpeed, acceleration, damage, breakForce, megModel, rotationSpeed, jumpHeight);
+        this.pitchSpeed = pitchSpeed;
+        this.maxPitch = maxPitch;
+        this.minPitch = minPitch;
+        this.minFallSpeed = minFallSpeed;
+    }
+
+    @Override
+    public Airplane createVehicle(Location location, String plate) {
+        World world = location.getWorld();
+        if (world == null)
+            throw new IllegalArgumentException("invalid location: " + location);
+
+        ActiveModel model = getMegModel();
+        model.setClamp(location.getYaw());
+
+        Airplane airplane = new Airplane(this, plate, model);
+
+        AirplaneEntity entity = new AirplaneEntity(location, airplane);
+        ((CraftWorld) world).getHandle().addEntity(entity);
+        model.setClamp(0);
+
+        ModeledEntity modeledEntity = ModelEngineAPI.api.getModelManager().createModeledEntity(entity.getBukkitEntity());
+        modeledEntity.addActiveModel(model);
+        modeledEntity.setInvisible(true);
+        modeledEntity.detectPlayers();
+
+        IMountHandler mountHandler = modeledEntity.getMountHandler();
+        mountHandler.setSteerable(true);
+        mountHandler.setCanCarryPassenger(true);
+
+        airplane.markEntity();
+        return airplane;
+    }
+
+    @Override
+    public @NotNull Map<String, Object> serialize() {
+        Map<String, Object> map = super.serialize();
+        map.put("pitch", Map.of(
+                "speed", pitchSpeed,
+                "max", maxPitch,
+                "min", minPitch
+        ));
+        return map;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static AirplaneModel deserialize(Map<String, Object> args) {
+        Map<String, Object> pitch = (Map<String, Object>) args.get("pitch");
+        return new AirplaneModel(
+                Vehicles.getInstance(),
+                (String) args.get("name"),
+                ((Number) args.getOrDefault("max-speed", 0d)).doubleValue(),
+                (Map<Double, Double>) args.get("acceleration"),
+                (Map<Double, Double>) args.get("damage"),
+                (Map<Double, Double>) args.get("break-force"),
+                (String) args.get("meg-model"),
+                ((Number) args.getOrDefault("rotation-speed", 0f)).floatValue(),
+                ((Number) args.getOrDefault("jump-height", 0.6f)).floatValue(),
+                ((Number) pitch.getOrDefault("min", 90f)).floatValue(),
+                ((Number) pitch.getOrDefault("max", 90f)).floatValue(),
+                ((Number) pitch.getOrDefault("speed", 0f)).floatValue(),
+                ((Number) args.getOrDefault("min-fall-speed", 0f)).floatValue()
+        );
+    }
+}
---
+++ api/src/main/java/com/wizardlybump17/vehicles/api/vehicle/airplane/Airplane.java
@@ -0,0 +1,144 @@
+package com.wizardlybump17.vehicles.api.vehicle.airplane;
+
+import com.ticxo.modelengine.api.model.ActiveModel;
+import com.wizardlybump17.vehicles.api.entity.AirplaneEntity;
+import com.wizardlybump17.vehicles.api.model.airplane.AirplaneModel;
+import com.wizardlybump17.vehicles.api.vehicle.Vehicle;
+import org.bukkit.Location;
+import org.bukkit.block.BlockFace;
+import org.bukkit.craftbukkit.v1_17_R1.entity.CraftEntity;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Airplane extends Vehicle<AirplaneModel> {
+
+    public static final long SPEED_TIMEOUT = 100;
+    public static final long PITCH_TIMEOUT = 100;
+    public static final long DAMAGE_DELAY = 1000;
+
+    private long lastSpeedUpdate;
+    private long lastPitchUpdate;
+    private final Map<Entity, Long> damagedEntities = new HashMap<>();
+
+    public Airplane(AirplaneModel model, String plate, ActiveModel megModel) {
+        super(model, plate, megModel);
+    }
+
+    @Override
+    public void move(Player player, double xxa, double zza) {
+        if (zza <= 0 || !player.equals(getDriver()))
+            return;
+
+        Entity entity = getEntity();
+
+        if (System.currentTimeMillis() - lastSpeedUpdate > SPEED_TIMEOUT)
+            setSpeed(0);
+
+        lastSpeedUpdate = System.currentTimeMillis();
+
+        setSpeed(Math.min(getModel().getMaxSpeed(), getSpeed() + getModel().getAcceleration(getSpeed())));
+
+        Location location = entity.getLocation();
+        if (System.currentTimeMillis() - lastPitchUpdate > PITCH_TIMEOUT)
+            location.setPitch(0);
+
+        Vector direction = location.getDirection();
+        direction.multiply(getSpeed());
+
+        entity.setVelocity(direction);
+    }
+
+    @Override
+    public void rotate(Player player, double xxa, double zza) {
+        if (xxa == 0 || zza <= 0 || !player.equals(getDriver()))
+            return;
+
+        AirplaneEntity entity = (AirplaneEntity) ((CraftEntity) getEntity()).getHandle();
+
+        float yaw = entity.getBukkitYaw();
+        yaw += xxa > 0 ? -getModel().getRotationSpeed() : getModel().getRotationSpeed();
+        entity.getBukkitEntity().setRotation(yaw, System.currentTimeMillis() - lastPitchUpdate > PITCH_TIMEOUT ? 0 : entity.getBukkitEntity().getLocation().getPitch());
+    }
+
+    @Override
+    public void jump(Player player, double xxa, double zza) {
+        if (zza <= 0 || !player.equals(getDriver()))
+            return;
+
+        AirplaneEntity entity = (AirplaneEntity) ((CraftEntity) getEntity()).getHandle();
+        Location location = entity.getBukkitEntity().getLocation();
+
+        float pitch = location.getPitch();
+        if (System.currentTimeMillis() - lastPitchUpdate > PITCH_TIMEOUT)
+            pitch = 0;
+
+        pitch -= getModel().getPitchSpeed();
+        pitch = Math.max(getModel().getMinPitch(), pitch);
+        entity.getBukkitEntity().setRotation(location.getYaw(), pitch);
+
+        lastPitchUpdate = System.currentTimeMillis();
+    }
+
+    @Override
+    public void shift(Player player, double xxa, double zza) {
+        if (zza <= 0 || !player.equals(getDriver()))
+            return;
+
+        AirplaneEntity entity = (AirplaneEntity) ((CraftEntity) getEntity()).getHandle();
+        Location location = entity.getBukkitEntity().getLocation();
+
+        float pitch = location.getPitch();
+        if (System.currentTimeMillis() - lastPitchUpdate > PITCH_TIMEOUT)
+            pitch = 0;
+
+        pitch += getModel().getPitchSpeed();
+        pitch = Math.min(getModel().getMaxPitch(), pitch);
+        entity.getBukkitEntity().setRotation(location.getYaw(), pitch);
+
+        lastPitchUpdate = System.currentTimeMillis();
+    }
+
+    @Override
+    public void onDamage(Player player) {
+    }
+
+    @Override
+    public void onInteract(Player player) {
+        addEntity(player);
+    }
+
+    @Override
+    public void onCollide(Entity entity) {
+        if (getSpeed() == 0 || !(entity instanceof LivingEntity living) || damagedEntities.getOrDefault(entity, System.currentTimeMillis()) > System.currentTimeMillis() || System.currentTimeMillis() - lastSpeedUpdate > DAMAGE_DELAY)
+            return;
+
+        living.damage(getModel().getDamage(getSpeed()), getEntity());
+        damagedEntities.put(entity, System.currentTimeMillis() + DAMAGE_DELAY);
+    }
+
+    @Override
+    public void check() {
+        if (System.currentTimeMillis() - lastSpeedUpdate > SPEED_TIMEOUT)
+            setSpeed(0);
+
+        if (getSpeed() > getModel().getMinFallSpeed())
+            return;
+
+        setSpeed(getModel().getMaxSpeed());
+        lastSpeedUpdate = System.currentTimeMillis();
+
+        AirplaneEntity entity = (AirplaneEntity) ((CraftEntity) getEntity()).getHandle();
+        Location location = entity.getBukkitEntity().getLocation();
+        location.setPitch(getModel().getMaxPitch());
+
+        Vector vector = location.getDirection().multiply(getModel().getMaxSpeed());
+
+        if (location.getBlock().getRelative(BlockFace.DOWN).isPassable())
+            entity.getBukkitEntity().setVelocity(vector);
+    }
+}
